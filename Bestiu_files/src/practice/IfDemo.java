6db22c7ee690ef2f4c356d69c2d92f1e37ecc1aa
MY PC <MY PC@DESKTOP-PGU1DP2.lan>
2024-09-10 22:21:43 +0530

    FOURTH COMMIT

---
+++ Bestiu_files/src/practice/IfDemo.java
@@ -0,0 +1,16 @@
+package practice;
+
+public class IfDemo {
+
+	public static void main(String[] args) {
+		int age=7;
+		if(age>=18) {
+			System.out.println("Eligible to Vote");
+		}
+		else {
+			System.out.println("Not Eligible to Vote");
+		}
+
+	}
+
+}
