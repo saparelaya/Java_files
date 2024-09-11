1eb8eeecbad0a4e416289d1cfc56328ef944d068
MY PC <MY PC@DESKTOP-PGU1DP2.lan>
2024-09-11 21:51:47 +0530

    FIFTH COMMIT

---
+++ Bestiu_files/src/practice/ReturnDemo.java
@@ -0,0 +1,13 @@
+package practice;
+
+public class ReturnDemo {
+
+	public static void main(String[] args) {
+		System.out.println(sub(65,34));
+	}
+		static int sub(int a,int b)
+		{
+			return a-b;
+		}
+
+}
