33ecf5f1d3f0b67c2d0c65bb8c8ccc5a62ce35c9
MY PC <MY PC@DESKTOP-PGU1DP2.lan>
2024-09-11 21:26:10 +0530

    FIFTH COMMIT

---
+++ Bestiu_files/src/practice/ForEachLoop.java
@@ -0,0 +1,12 @@
+package practice;
+
+public class ForEachLoop {
+
+	public static void main(String[] args) {
+		int num[]= {8,25,43,56};
+		for (int print:num)
+		{
+			System.out.println(print);
+		}
+	}
+}
