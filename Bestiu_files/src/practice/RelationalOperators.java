0fc067080dd085124f17a49baa9b713c920ea27e
MY PC <MY PC@DESKTOP-PGU1DP2.lan>
2024-09-12 22:56:23 +0530

    SIXTH COMMIT

---
+++ Bestiu_files/src/practice/RelationalOperators.java
@@ -0,0 +1,17 @@
+package practice;
+
+public class RelationalOperators {
+
+	public static void main(String[] args) {
+		int a=65,b=13;
+		
+		System.out.println("a==b:" +(a==b));
+		System.out.println("a!=b:" +(a!=b));
+		System.out.println("a>b:" +(a>b));
+		System.out.println("a<b:" +(a<b));
+		System.out.println("a>=b:" +(a>=b));
+		System.out.println("a<=b:" +(a<=b));
+
+	}
+
+}
