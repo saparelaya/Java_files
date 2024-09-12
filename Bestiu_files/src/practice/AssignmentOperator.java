259fb941b113bdec518ad68d69ae4b92c0050418
MY PC <MY PC@DESKTOP-PGU1DP2.lan>
2024-09-12 21:50:15 +0530

    SIXTH COMMIT

---
+++ Bestiu_files/src/practice/AssignmentOperator.java
@@ -0,0 +1,26 @@
+package practice;
+
+public class AssignmentOperator {
+
+	public static void main(String[] args) {
+        int a=25;
+		
+	
+		a+=15; 
+		System.out.println("a after += :" +a);
+		
+		a-=5; 
+		System.out.println("a after -= :"  +a);
+		
+		a*=10;  
+		System.out.println("a after *= :"  +a);
+		
+		a/=40;
+		System.out.println("a after /= :" +a);
+		
+		a%=6; 
+		System.out.println("a after %= :" +a);
+
+	}
+
+}
