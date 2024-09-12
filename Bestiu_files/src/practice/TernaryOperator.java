473f3d438f6c1aaefcc751d09126dde64b553741
MY PC <MY PC@DESKTOP-PGU1DP2.lan>
2024-09-12 23:34:55 +0530

    SIXTH COMMIT

---
+++ Bestiu_files/src/practice/TernaryOperator.java
@@ -0,0 +1,12 @@
+package practice;
+
+public class TernaryOperator {
+
+	public static void main(String[] args) {
+		int a=24,b=15;
+		int max=(a>b) ? a:b;
+		System.out.println("The max value:" +max);
+		
+	}
+
+}
