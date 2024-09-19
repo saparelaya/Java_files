f462968e0291bc8a10913096c8fee8c1940000c0
MY PC <MY PC@DESKTOP-PGU1DP2.lan>
2024-09-19 23:41:08 +0530

    SEVENTH COMMIT

---
+++ Bestiu_files/src/org/tns/capgemini/c2tc/oops/HeirarchicalInheritance.java
@@ -0,0 +1,38 @@
+package org.tns.capgemini.c2tc.oops;
+class Animal2{
+	String name="Animal";
+	void eat() {
+		System.out.println(name+ " is eating ");
+	}
+}
+class Dog2 extends Animal2{
+	void bark() {
+		System.out.println("Dog is barking ");
+	}
+}
+class cat extends Animal2{
+	void meow() {
+		System.out.println("cat is meowing ");
+	}
+}
+class Elephant extends Animal2{
+	void trumphet() {
+		System.out.println("Elephant is trumpeting ");
+	}
+}
+public class HeirarchicalInheritance {
+
+	public static void main(String[] args) {
+		Dog2 dog=new Dog2();
+		cat cat=new cat();
+		Elephant ele=new Elephant();
+		
+		dog.eat();
+		cat.eat();
+		ele.eat();
+		
+		dog.bark();
+		cat.meow();
+		ele.trumphet();
+	}
+}
