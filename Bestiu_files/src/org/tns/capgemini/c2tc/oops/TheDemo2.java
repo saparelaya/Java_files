1a60371fa7e997977e081e53998843a94deef517
MY PC <MY PC@DESKTOP-PGU1DP2.lan>
2024-09-21 23:00:34 +0530

    NINTH COMMIT

--- Bestiu_files/src/org/tns/capgemini/c2tc/oops/TheDemo2.java
+++ Bestiu_files/src/org/tns/capgemini/c2tc/oops/TheDemo2.java
@@ -1,9 +1,25 @@
 package org.tns.capgemini.c2tc.oops;
+class Product {
+    double price;
 
+    Product(double price) {
+        this.price = price;
+    }
+
+    double calculateDiscount() {
+        return this.price * 0.10; 
+    }
+
+    double calculateFinalPrice() {
+        return this.price - this.calculateDiscount();  
+    }
+}
 public class TheDemo2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		 Product product = new Product(500.0);
+	        
+	        System.out.println("Final Price: $" + product.calculateFinalPrice());
 
 	}
 
