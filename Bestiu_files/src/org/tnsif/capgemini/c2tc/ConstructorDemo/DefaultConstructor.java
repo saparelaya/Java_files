6630cc29fa614ea0d75f13fd3e8e58f52eb2874c
MY PC <MY PC@DESKTOP-PGU1DP2.lan>
2024-09-26 11:57:21 +0530

    TEENTH COMMIT

---
+++ Bestiu_files/src/org/tnsif/capgemini/c2tc/ConstructorDemo/DefaultConstructor.java
@@ -0,0 +1,22 @@
+package org.tnsif.capgemini.c2tc.ConstructorDemo;
+class MyClass1
+{
+	String color;
+	int cost;
+	String brand;
+
+MyClass1()
+{
+	color="Black";
+	cost=500;
+	brand="LV";
+}
+}
+public class DefaultConstructor {
+
+	public static void main(String[] args) {
+		MyClass1 obj=new MyClass1();
+		System.out.println(obj.color + " " + obj.cost + "  " + obj.brand);
+	}
+
+}
