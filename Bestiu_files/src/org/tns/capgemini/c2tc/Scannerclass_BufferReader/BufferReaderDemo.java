a3f7f71a8ce6c8467ccf7de7bb0ef809359fa062
MY PC <MY PC@DESKTOP-PGU1DP2.lan>
2024-09-26 13:26:46 +0530

    ELEVENTH COMMIT

---
+++ Bestiu_files/src/org/tns/capgemini/c2tc/Scannerclass_BufferReader/BufferReaderDemo.java
@@ -0,0 +1,17 @@
+package org.tns.capgemini.c2tc.Scannerclass_BufferReader;
+import java.io.BufferedReader;
+import java.io.FileReader; 
+import java.io.IOException;
+public class BufferReaderDemo {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br=new BufferedReader(new FileReader("C:\\Users\\MY PC\\OneDrive\\Documents\\coverletter.laya.docx"));
+		String data="";
+		while((data=br.readLine())!=null) 
+			{
+			System.out.println(data);
+			}
+		br.close();
+
+	}
+
+}
