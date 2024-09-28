398a26ba2940e2f190c53617a8d013d932b0f4bb
MY PC <MY PC@DESKTOP-PGU1DP2.lan>
2024-09-28 22:35:28 +0530

    TWELTH COMMIT

---
+++ Bestiu_files/src/org/tns/capgemini/c2tc/StaticDemo/StaticBlock.java
@@ -0,0 +1,21 @@
+package org.tns.capgemini.c2tc.StaticDemo;
+class StaticBlockDemo{
+	public void display() {
+		System.out.println("Instance Block-1");
+	}
+	static {
+		System.out.println("Static Block-1");
+	}
+	static {
+		System.out.println("Static Block-2");
+	}
+}
+public class StaticBlock {
+
+	public static void main(String[] args) {
+		StaticBlockDemo obj=new StaticBlockDemo();
+		obj.display();
+
+	}
+
+}
