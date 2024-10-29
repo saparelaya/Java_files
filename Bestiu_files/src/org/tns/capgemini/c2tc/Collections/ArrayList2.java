4ee39fb93954dab9ca94954b04049841abff6115
MY PC <MY PC@DESKTOP-PGU1DP2.lan>
2024-10-29 13:45:30 +0530

    20 COMMIT

---
+++ Bestiu_files/src/org/tns/capgemini/c2tc/Collections/ArrayList2.java
@@ -0,0 +1,15 @@
+package org.tns.capgemini.c2tc.Collections;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayList2 {
+
+	public static void main(String[] args) {
+			List<Integer> al=new ArrayList<>();
+			for(int i=1;i<=5;i++)
+				al.add(i);
+			System.out.println(al);
+			al.remove(3);
+			System.out.println(al);
+	}
+}
