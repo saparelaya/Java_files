b2ed023ba800ccc0b58133ddaf19e0448d1b1cc2
MY PC <MY PC@DESKTOP-PGU1DP2.lan>
2024-11-10 15:27:11 +0530

    23 COMMIT

---
+++ Bestiu_files/src/org/tns/capgemini/c2tc/Generics/AfterGenerics.java
@@ -0,0 +1,14 @@
+package org.tns.capgemini.c2tc.Generics;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AfterGenerics {
+
+	public static void main(String[] args) {
+		List<String> list1=new ArrayList<>();
+		list1.add("cherry");
+		list1.add("apple");
+
+	}
+
+}
